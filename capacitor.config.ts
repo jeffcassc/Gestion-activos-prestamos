e1b4460e7aa3a03238ae9c88adb4038657e8d77a
jeffcassc <yefreycassinia16@gmail.com>
2024-10-20 14:26:39 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Front/Gestion-activos-prestamos',
+  webDir: 'www'
+};
+
+export default config;
